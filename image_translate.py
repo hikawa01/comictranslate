afbb1c27fda74c82a059ed164496574ac0ff197d
王昭智 <georgewang@georgedeMacBook-Pro.local>
2023-10-02 17:34:42 +0800

    Initial commit

--- image_translate.py
+++ image_translate.py
@@ -7,7 +7,6 @@ from PIL import Image, ImageDraw, ImageFont
 def translate_images(input_folder, output_folder):
     # 設定 Google Cloud Vision API 的認證金鑰路徑
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/georgewang/Documents/key/GCP_STUDY_TRANSLATE/fresh-bloom-293108-914e1d599383.json"
-
     client = vision.ImageAnnotatorClient()
     translate_client = translate.Client()
 
@@ -31,8 +30,8 @@ def translate_images(input_folder, output_folder):
                 # 使用PIL處理圖片
                 img = Image.open(file_path)
                 draw = ImageDraw.Draw(img)
-                font_path = "/Users/georgewang/Library/Fonts/NotoSansTC-Regular.ttf" # 設定字體檔路徑
-                font = ImageFont.truetype(font_path, 15) # 設定使用的字體
+                font_path = "/Users/georgewang/Library/Fonts/NotoSansTC-Regular.ttf"  # 設定字體檔路徑
+                font = ImageFont.truetype(font_path, 15)  # 設定使用的字體
 
                 for text in texts[1:]:
                     vertices = [(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices]
@@ -43,11 +42,10 @@ def translate_images(input_folder, output_folder):
                 base_filename = os.path.splitext(filename)[0]  # 不包含副檔名的檔名
                 output_path = os.path.join(output_folder, base_filename + ".png")
                 img.save(output_path)
-
                 processed_files += 1
                 print(f"Processed {processed_files}/{total_files} files...")
 
 if __name__ == "__main__":
-    input_folder = "/Users/georgewang/dev/PythonDev/MangaTranslate/raw" # 生肉路徑
-    output_folder = "/Users/georgewang/dev/PythonDev/MangaTranslate/cooked" # 熟肉路徑
+    input_folder = "/Users/georgewang/dev/PythonDev/MangaTranslate/raw"  # 生肉路徑
+    output_folder = "/Users/georgewang/dev/PythonDev/MangaTranslate/cooked"  # 熟肉路徑
     translate_images(input_folder, output_folder)
